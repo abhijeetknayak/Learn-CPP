1035848be48bcc00af3a8c6ce065dad235b9dc35
Abhijeet Nayak <abhijeetknayak@gmail.com>
2020-07-05 13:55:58 +0530

    Polynomial Class Project : Header file

--- Learn-Cpp/src/polynomial.h
+++ Learn-Cpp/src/polynomial.h
@@ -9,13 +9,44 @@ namespace abhijeet1A
 	{
 	public:
 		typedef std::size_t size_type;
-		static const size_type MAXDEGREE = 10;
 
 		/* Constructor */
 		Polynomial();
+		Polynomial(double a0);
+		Polynomial(const Polynomial& poly);
+		Polynomial(size_type deg = 0);
+
+		/* Destructor */
+		~Polynomial();
+
+		/* Friend Function */
+		friend std::ostream& operator <<(std::ostream& out, const Polynomial& target);
+
+		void operator =(const Polynomial& poly);
+
+		/* Modifier member functions */
+		void add_to_coef(double amount, size_type k);
+		void assign_coef(double new_coefficient, size_type k);
+		void clear(void);
+		void reserve(size_t number);
+
+		/* Constant Member functions */
+		double coefficient(size_type k) const;
+		size_type get_degree( ) const;
+		size_type next_term(size_type k) const;
+
+		/* Evaluation Functions */
+		double eval(double x) const;
+		double operator ( )(double x) const;
+
 	private:
-		float coefficients[MAXDEGREE];
+		double * coefficients;
+		size_type degree;
 	};
+
+
+
+
 }
 
 #endif
