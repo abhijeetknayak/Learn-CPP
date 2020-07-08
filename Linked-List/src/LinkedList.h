71e3773a404d9d108e96f3b12f5eb7af1dd52b62
Abhijeet Nayak <abhijeetknayak@gmail.com>
2020-07-08 12:13:54 +0530

    Node class for Linked lists

---
+++ Linked-List/src/LinkedList.h
@@ -0,0 +1,33 @@
+#ifndef LINKED_LIST_HEADER
+#define LINKED_LIST_HEADER
+
+#include <iostream>
+
+
+
+class Node
+{
+    public:
+	    typedef double value_type;
+
+
+
+
+
+
+    private:
+	    value_type value;
+	    Node * next_node;
+	    Node * prev_node;
+
+
+
+
+
+
+
+
+};
+
+
+#endif
