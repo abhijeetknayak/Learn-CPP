0b9b4be35e38e9045be035789192d33e4cc7d540
Abhijeet Nayak <abhijeetknayak@gmail.com>
2020-07-08 19:54:35 +0530

    Linked List : Node class definition

--- Linked-List/src/LinkedList.h
+++ Linked-List/src/LinkedList.h
@@ -10,15 +10,25 @@ class Node
     public:
 	    typedef double value_type;
 
+	    /* Constructor */
+	    Node(const value_type& value = value_type(), Node * link = NULL);
+
+	    void set_data(const value_type& value) { data = value; }
+	    void set_link(Node * link = NULL) { next_node = link; }
+
+	    value_type get_data() const { return data; }
+	    const Node * get_link() const { return next_node; }
+	    Node * get_link() { return next_node; }
+
 
 
 
 
 
     private:
-	    value_type value;
+	    value_type data;
 	    Node * next_node;
-	    Node * prev_node;
+//	    Node * prev_node;
 
 
 
